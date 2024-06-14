2af0d110b76c8087527628cecac8c542417138c3
Yunosuke Sato <yunosukesato04@gmail.com>
2024-06-14 12:27:41 +0900

    Toggle display by session data

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Todo App</title>
+      <meta name="description" content="Full stack todo app" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
