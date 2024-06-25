130005a0f8efb8e9eab8e2babf365d185d8fd6c1
Yunosuke Sato <yunosukesato04@gmail.com>
2024-06-25 12:31:26 +0900

    Fix: fix eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,11 +13,11 @@ const config = {
   rules: {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/no-unsafe-argument": "error",
-    "@typescript-eslint/no-unsafe-call": "error",
-    "@typescript-eslint/no-unsafe-return": "error",
-    "@typescript-eslint/no-unsafe-member-access": "error",
-    "@typescript-eslint/no-redundant-type-constituents": "error",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-redundant-type-constituents": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
